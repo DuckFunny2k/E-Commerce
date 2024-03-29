9a183812c7173efd3dc1f36308c8208f85d038b2
DuckFunny2k <lamducfpt2004@gmail.com>
2024-03-29 12:50:18 +0700

    BE_handleUser_handleProduct

--- controllers/product.js
+++ controllers/product.js
@@ -1,54 +1,143 @@
-const Product = require('../models/product')
-const asyncHandler = require('express-async-handler')
-const slugify = require('slugify')
+const { response } = require("express");
+const Product = require("../models/product");
+const asyncHandler = require("express-async-handler");
+const slugify = require("slugify");
 
 const createProduct = asyncHandler(async (req, res) => {
-    if (Object.keys(req.body).length === 0) throw new Error('Missing inputs')
-    if (req.body && req.body.title) req.body.slug = slugify(req.body.title)
-    const newProduct = await Product.create(req.body)
-    return res.status(200).json({
-        success: newProduct ? true : false,
-        createdProduct: newProduct ? newProduct : 'Cannot create new product'
-    })
-})
+  if (Object.keys(req.body).length === 0) throw new Error("Missing inputs");
+  if (req.body && req.body.title) req.body.slug = slugify(req.body.title);
+  const newProduct = await Product.create(req.body);
+  return res.status(200).json({
+    success: newProduct ? true : false,
+    createdProduct: newProduct ? newProduct : "Cannot create new product",
+  });
+});
 const getProduct = asyncHandler(async (req, res) => {
-    const { pid } = req.params
-    const product = await Product.findById(pid)
-    return res.status(200).json({
-        success: product ? true : false,
-        productData: product ? product : 'Cannot get product'
-    })
-})
+  const { pid } = req.params;
+  const product = await Product.findById(pid);
+  return res.status(200).json({
+    success: product ? true : false,
+    productData: product ? product : "Cannot get product",
+  });
+});
 // Filtering, sorting & pagination
 const getProducts = asyncHandler(async (req, res) => {
-    const products = await Product.find()
+  const queries = { ...req.query };
+  // Tách các trường đặc biệt ra khỏi query
+  const excludeFields = ["limit", "sort", "page", "fields"];
+  excludeFields.forEach((el) => delete queries[el]);
+
+  // format lai cac operators cho dung cu phap mongoose
+  let queryString = JSON.stringify(queries);
+  queryString = queryString.replace(
+    /\b(gte|gt|lt|lte)\b/g,
+    (macthedEl) => `$${macthedEl}`
+  );
+  const formatedQueries = JSON.parse(queryString);
+  // filtering
+  if (queries?.title)
+    formatedQueries.title = { $regex: queries.title, $options: "i" };
+  let queryCommand = Product.find(formatedQueries);
+
+  //sorting
+  if (req.query.sort) {
+    const sortBy = req.query.sort.split(",").join(" ");
+    queryCommand = queryCommand.sort(sortBy);
+  }
+
+  // fields limiting
+  if (req.query.fields) {
+    const fields = req.query.fields.split(",").join(" ");
+    queryCommand = queryCommand.select(fields);
+  }
+  // pagination
+
+  const page = req.query.page || 1;
+  const limit = req.query.limit || process.env.LIMIT_PRODUCT;
+  const skip = (page - 1) * limit;
+  queryCommand.skip(skip).limit(limit);
+
+  queryCommand.exec(async (err, response) => {
+    if (err) throw new Error(err.message);
+    const counts = await Product.find(formatedQueries.countDocuments());
     return res.status(200).json({
-        success: products ? true : false,
-        productDatas: products ? products : 'Cannot get products'
-    })
-})
+      success: response ? true : false,
+      counts,
+      products: response ? products : "Cannot get products",
+    });
+  });
+});
+
 const updateProduct = asyncHandler(async (req, res) => {
-    const { pid } = req.params
-    if (req.body && req.body.title) req.body.slug = slugify(req.body.title)
-    const updatedProduct = await Product.findByIdAndUpdate(pid, req.body, { new: true })
-    return res.status(200).json({
-        success: updatedProduct ? true : false,
-        updatedProduct: updatedProduct ? updatedProduct : 'Cannot update product'
-    })
-})
+  const { pid } = req.params;
+  if (req.body && req.body.title) req.body.slug = slugify(req.body.title);
+  const updatedProduct = await Product.findByIdAndUpdate(pid, req.body, {
+    new: true,
+  });
+  return res.status(200).json({
+    success: updatedProduct ? true : false,
+    updatedProduct: updatedProduct ? updatedProduct : "Cannot update product",
+  });
+});
 const deleteProduct = asyncHandler(async (req, res) => {
-    const { pid } = req.params
-    const deletedProduct = await Product.findByIdAndDelete(pid)
-    return res.status(200).json({
-        success: deletedProduct ? true : false,
-        deletedProduct: deletedProduct ? deletedProduct : 'Cannot delete product'
-    })
-})
+  const { pid } = req.params;
+  const deletedProduct = await Product.findByIdAndDelete(pid);
+  return res.status(200).json({
+    success: deletedProduct ? true : false,
+    deletedProduct: deletedProduct ? deletedProduct : "Cannot delete product",
+  });
+});
+
+const ratings = asyncHandler(async (req, res) => {
+  const { _id } = req.user;
+  const { star, comment, pid } = req.body;
+  if (!star || !pid) throw new Error("missing_inputs");
+  const ratingProduct = await Product.findById(pid);
+  const alreadyRating = ratingProduct?.ratings?.some(
+    (el) => el.postedBy.toString() === _id
+  );
+  if (alreadyRating) {
+    await Product.updateOne(
+      {
+        ratings: { $elemMatch: alreadyRating },
+      },
+      {
+        $set: { "ratings.$.star": star, "ratings.$.comment": comment },
+      },
+      { new: true }
+    );
+  } else {
+    const response = await Product.findByIdAndUpdate(
+      pid,
+      {
+        $push: { ratings: { star, comment, postedBy: _id } },
+      },
+      { new: true }
+    );
+  }
+
+  // sum ratings
+  const updatedProduct = await Product.findById(pid);
+  const ratingCount = updatedProduct.ratings.length;
+  const sumRatings = updatedProduct.ratings.reduce(
+    (sum, el) => sum + +el.start,
+    0
+  );
+  updatedProduct.totalRatings =
+    Math.round((sumRatings * 10) / ratingCount) / 10;
+
+  await updatedProduct.save();
+
+  return res.status(200).json({
+    status: true,
+  });
+});
 
 module.exports = {
-    createProduct,
-    getProduct,
-    getProducts,
-    updateProduct,
-    deleteProduct
-}
+  createProduct,
+  getProduct,
+  getProducts,
+  updateProduct,
+  deleteProduct,
+  ratings,
+};
